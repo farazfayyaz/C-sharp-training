35727f879086406ee3952bd2744c152cd3695e49
farazfayyaz <farazfayyazwork@gmail.com>
2024-02-25 22:49:11 -0500

    updating challenge

--- renewalRate_ifElsePractice/CsharpProjects/TestProject/Program.cs
+++ renewalRate_ifElsePractice/CsharpProjects/TestProject/Program.cs
@@ -5,5 +5,10 @@ int discountPercentage = 0;
 // Your code goes here
 
 if (daysUntilExpiration <= 10) {
-    Console.WriteLine("Your subscription will expire soon. Renew now!")
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+if (daysUntilExpiration <= 5) {
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    Console.WriteLine($"Renew now and save 10%!");
 }
