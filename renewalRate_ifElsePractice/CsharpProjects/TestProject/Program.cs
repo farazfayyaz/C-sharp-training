4201225646b794496f09920afc3720e22ecb36c9
farazfayyaz <farazfayyazwork@gmail.com>
2024-02-28 22:57:34 -0500

    updating challenge

--- renewalRate_ifElsePractice/CsharpProjects/TestProject/Program.cs
+++ renewalRate_ifElsePractice/CsharpProjects/TestProject/Program.cs
@@ -2,24 +2,26 @@
 int daysUntilExpiration = random.Next(12);
 int discountPercentage = 0;
 
-// Your code goes here
-
-if (daysUntilExpiration <= 10) {
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
     Console.WriteLine("Your subscription will expire soon. Renew now!");
 }
 
-if (daysUntilExpiration <= 5) {
-    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
-    Console.WriteLine($"Renew now and save 10%!");
-    discountPercentage += 10;
-}
-
-if (daysUntilExpiration == 1) {
-    Console.WriteLine($"Your subscription expires within a day!");
-    Console.WriteLine($"Renew now and save 20%!");
-    discountPercentage += 20;
-}
-
-if (daysUntilExpiration == 0) {
-    Console.WriteLine("Your subscription has expired.");
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
 }
