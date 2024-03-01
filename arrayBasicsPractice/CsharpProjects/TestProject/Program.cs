aeb2155f81d872c5fffa2df0a97dbaef5208fce3
farazfayyaz <farazfayyazwork@gmail.com>
2024-02-29 22:20:25 -0500

    committing array practice

---
+++ arrayBasicsPractice/CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] fraudulentOrderIDs = new string[3]; //declaring an array that holds 3 strings
+
+//assigning index value with a string
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+
+//access an array value 
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
