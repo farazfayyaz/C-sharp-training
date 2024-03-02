b85af28f2db7723fca52975c4acc26bb3e0aef61
farazfayyaz <farazfayyazwork@gmail.com>
2024-03-01 22:33:03 -0500

    array practice changes

--- arrayBasicsPractice/CsharpProjects/TestProject/Program.cs
+++ arrayBasicsPractice/CsharpProjects/TestProject/Program.cs
@@ -1,15 +1,27 @@
-﻿string[] fraudulentOrderIDs = new string[3]; //declaring an array that holds 3 strings
+﻿// string[] fraudulentOrderIDs = new string[3]; //declaring an array that holds 3 strings
 
-//assigning index value with a string
-fraudulentOrderIDs[0] = "A123";
-fraudulentOrderIDs[1] = "B456";
-fraudulentOrderIDs[2] = "C789";
+// //assigning index value with a string
+// fraudulentOrderIDs[0] = "A123";
+// fraudulentOrderIDs[1] = "B456";
+// fraudulentOrderIDs[2] = "C789";
 
-//access an array value 
-Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
-Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
-Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+// //access an array value 
+// Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+// Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
 
-fraudulentOrderIDs[0] = "F000";
+// fraudulentOrderIDs[0] = "F000";
 
-Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+/* Multi line comment */
+
+string[] arr2 = { "A123", "B456", "C789" }; //initialize array during declaration
+
+Console.WriteLine($"First: {arr2[0]}");
+Console.WriteLine($"Second: {arr2[1]}");
+Console.WriteLine($"Third: {arr2[2]}");
+
+arr2[0] = "F000";
+
+Console.WriteLine($"Reassign First: {arr2[0]}");
