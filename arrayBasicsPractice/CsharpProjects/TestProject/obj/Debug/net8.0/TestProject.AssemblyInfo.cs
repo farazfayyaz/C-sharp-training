354c644ed8f242ddf1e2bf2becf562b08784642c
farazfayyaz <farazfayyazwork@gmail.com>
2024-03-05 00:22:30 -0500

    array practice addition

--- arrayBasicsPractice/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ arrayBasicsPractice/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f42599b6dd381438384755763f6b990febeceee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f14debec1b94a16dfe0af67e40bdc373e9a4e3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
