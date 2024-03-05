5f14debec1b94a16dfe0af67e40bdc373e9a4e3a
farazfayyaz <farazfayyazwork@gmail.com>
2024-03-05 00:19:38 -0500

    array practice addition

--- arrayBasicsPractice/CsharpProjects/TestProject/Program.cs
+++ arrayBasicsPractice/CsharpProjects/TestProject/Program.cs
@@ -32,3 +32,13 @@ string[] names = { "Rowena", "Robin", "Bao" };
 foreach (string name in names) {
     Console.WriteLine(name);
 }
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+
+int sum = 0;
+
+foreach (int items in inventory) {
+    sum += items
+}
+
+Console.WriteLine($"We have {sum} items in inventory.");
