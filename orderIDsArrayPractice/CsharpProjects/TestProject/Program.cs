76a60cfe757db4b53fbed2ec64265618b03ca831
farazfayyaz <farazfayyazwork@gmail.com>
2024-03-05 22:48:29 -0500

    Committing OrderIDs array practice

---
+++ orderIDsArrayPractice/CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string id in orderIDs) {
+    //Console.WriteLine(id);
+
+    if (id.StartsWith("B")){
+        Console.WriteLine(id);
+    }
+}
