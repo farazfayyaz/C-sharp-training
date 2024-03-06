2c32621691cbc116fcbd378b1ef816b293a3891d
farazfayyaz <farazfayyazwork@gmail.com>
2024-03-06 00:14:13 -0500

    Committing Self guided if else and foreach practice

---
+++ gradeCalcPractice/CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+
+97 - 100   A+
+93 - 96    A
+90 - 92    A-
+87 - 89    B+
+83 - 86    B
+80 - 82    B-
+77 - 79    C+
+73 - 76    C
+70 - 72    C-
+67 - 69    D+
+63 - 66    D
+60 - 62    D-
+0  - 59    F
+
+
+Student         Grade
+
+Sophia:         92.2    A-
+Andrew:         89.6    B+
+Emma:           85.6    B
+Logan:          91.2    A-
+
+*/
+
+
+// initialize variables - graded assignments 
+int currentAssignments = 5; //num of assignments
+
+//sophia scores
+int sophia1 = 90;
+int sophia2 = 86;
+int sophia3 = 87;
+int sophia4 = 98;
+int sophia5 = 100;
+
+// andrew scores
+int andrew1 = 92;
+int andrew2 = 89;
+int andrew3 = 81;
+int andrew4 = 96;
+int andrew5 = 90;
+
+//emma scores
+int emma1 = 90;
+int emma2 = 85;
+int emma3 = 87;
+int emma4 = 98;
+int emma5 = 68;
+
+//logan scores
+int logan1 = 90;
+int logan2 = 95;
+int logan3 = 87;
+int logan4 = 88;
+int logan5 = 96;
+
+int sophiaSum = 0;
+int andrewSum = 0;
+int emmaSum = 0;
+int loganSum = 0;
+
+decimal sophiaScore;
+decimal andrewScore;
+decimal emmaScore;
+decimal loganScore;
