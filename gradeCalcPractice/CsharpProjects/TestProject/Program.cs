e988c61bb63305dbb3b7874ad42442e06c9cef65
farazfayyaz <farazfayyazwork@gmail.com>
2024-03-06 00:15:30 -0500

    Changes for Self guided if else and foreach practice

--- gradeCalcPractice/CsharpProjects/TestProject/Program.cs
+++ gradeCalcPractice/CsharpProjects/TestProject/Program.cs
@@ -75,3 +75,12 @@ sophiaScore = (decimal)sophiaSum / currentAssignments;
 andrewScore = (decimal)andrewSum / currentAssignments;
 emmaScore = (decimal)emmaSum / currentAssignments;
 loganScore = (decimal)loganSum / currentAssignments;
+
+Console.WriteLine("Student\t\tGrade\n");
+Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
+Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
+Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
+
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
