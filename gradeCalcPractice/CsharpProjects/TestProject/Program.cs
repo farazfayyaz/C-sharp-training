9f54491c75c6a95b5596676dbf16e8f35ea9272b
farazfayyaz <farazfayyazwork@gmail.com>
2024-03-08 22:14:10 -0500

    Changes for Self guided if else and foreach practice

--- gradeCalcPractice/CsharpProjects/TestProject/Program.cs
+++ gradeCalcPractice/CsharpProjects/TestProject/Program.cs
@@ -49,23 +49,15 @@ decimal emmaScore;
 decimal loganScore;
 
 //add all the variables together for the sum
-sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
-andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
-emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
-loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
+int sophiaSum = 0;
+
+decimal sophiaScore;
 
 //calculate average and store into score variable
 sophiaScore = (decimal)sophiaSum / currentAssignments;
-andrewScore = (decimal)andrewSum / currentAssignments;
-emmaScore = (decimal)emmaSum / currentAssignments;
-loganScore = (decimal)loganSum / currentAssignments;
 
 //print lines with formatting
 Console.WriteLine("Student\t\tGrade\n");
 Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
-Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
-Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
-Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
-
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
