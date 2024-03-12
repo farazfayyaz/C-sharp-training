b0094627774db9a2665511ddb15269be8e1ae353
farazfayyaz <farazfayyazwork@gmail.com>
2024-03-12 00:30:32 -0400

    update code with if else statements and cleaner code

--- gradeCalcPractice/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ gradeCalcPractice/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b1048cd5824be6da758af10564b9eff9d29e5f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfe739cde442d72a8f1188f1ab3019fe9e36a377")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
