da65b27bbb8e0533b6478695dfa099d8fc2b697b
farazfayyaz <farazfayyazwork@gmail.com>
2024-03-18 23:46:29 -0400

    Changes made for evalutation expressions practice file - added admin/manager checker

--- evaluateExpressionsPractice/CsharpProjects/TestProject/Program.cs
+++ evaluateExpressionsPractice/CsharpProjects/TestProject/Program.cs
@@ -1,4 +1,36 @@
-﻿Random coinSide = new Random(); // new random 
-int side = coinSide.Next(1, 3); //generates random number into variable
+﻿// Random coinSide = new Random(); // new random 
+// int side = coinSide.Next(1, 3); //generates random number into variable
 
-Console.WriteLine(side == 1 ? "Tails" : "Heads"); //string literal displays
+// Console.WriteLine(side == 1 ? "Tails" : "Heads"); //string literal displays
+
+
+
+string permission = "Admin|Manager";
+int level = 53;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
