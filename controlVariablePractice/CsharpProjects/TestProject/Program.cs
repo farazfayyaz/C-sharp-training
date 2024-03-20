e66dc3bbc99b3854df68ffbb795bd11b6e54f38e
farazfayyaz <farazfayyazwork@gmail.com>
2024-03-19 23:01:35 -0400

    changes to see variable and global scope differences

--- controlVariablePractice/CsharpProjects/TestProject/Program.cs
+++ controlVariablePractice/CsharpProjects/TestProject/Program.cs
@@ -1,6 +1,11 @@
 ﻿bool flag = true;
+int value = 0;
+
 if (flag)
 {
-    int value = 10;
+    
     Console.WriteLine($"Inside the code block: {value}");
 }
+
+value = 10;
+Console.WriteLine($"Outside the code block: {value}");
