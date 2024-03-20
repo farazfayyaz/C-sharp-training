086629f5f2441464e67af60fb15d915aef132c5e
farazfayyaz <farazfayyazwork@gmail.com>
2024-03-20 00:24:32 -0400

    changes to see variable and global scope differences

--- controlVariablePractice/CsharpProjects/TestProject/Program.cs
+++ controlVariablePractice/CsharpProjects/TestProject/Program.cs
@@ -1,11 +1,17 @@
-﻿bool flag = true;
-int value = 0;
+﻿int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
 
-if (flag)
+foreach (int number in numbers)
 {
-    
-    Console.WriteLine($"Inside the code block: {value}");
+    total += number;
+
+    if (number == 42)
+       found = true;
+
 }
 
-value = 10;
-Console.WriteLine($"Outside the code block: {value}");
+if (found) 
+    Console.WriteLine("Set contains 42");
+
+Console.WriteLine($"Total: {total}");
--- controlVariablePractice/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ controlVariablePractice/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ea40c8c9f40012ea5030e39bcab41b6fc502e90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e66dc3bbc99b3854df68ffbb795bd11b6e54f38e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
