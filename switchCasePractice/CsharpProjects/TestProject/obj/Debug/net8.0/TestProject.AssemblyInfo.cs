ef9894d35685bf12fc0080ca0db9945e2877239c
farazfayyaz <farazfayyazwork@gmail.com>
2024-03-22 00:12:23 -0400

    using switch statements to understand logic

--- switchCasePractice/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ switchCasePractice/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+086629f5f2441464e67af60fb15d915aef132c5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fa1c7c9ad2873530e33013898320853a1e17171")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
