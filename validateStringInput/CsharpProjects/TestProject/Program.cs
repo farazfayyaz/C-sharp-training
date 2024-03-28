a538f8b4f9d7f06c05c5a529bd38a4ab3e58aa47
farazfayyaz <farazfayyazwork@gmail.com>
2024-03-27 23:56:17 -0400

    committing looping logic practice - validating a string input

---
+++ validateStringInput/CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,45 @@
+﻿string? userInput;
+string input = "";
+
+bool validInput = false;
+
+Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+
+do
+{
+
+    userInput = Console.ReadLine();
+
+    if(userInput != null)
+    {
+        input = userInput.Trim().ToLower();
+    }
+
+    if(validInput == false)
+    {
+        if(input == "administrator")
+        {
+            Console.WriteLine("Your input value (Administrator) has been accepted.");
+            validInput = true;
+        }
+        else if (input == "manager")
+        {
+            Console.WriteLine("Your input value (Manager) has been accepted.");
+            validInput = true;
+        }
+        else if (input == "user")
+        {
+            Console.WriteLine("Your input value (User) has been accepted.");
+            validInput = true;
+        }
+        else
+        {
+            Console.WriteLine($"The role name that you entered, \"{input}\" is not valid. Enter your role name (Administrator, Manager, or User)");
+        }
+    }
+    else 
+    {
+        Console.WriteLine("Sorry, you entered an invalid number, please try again"); //otherwise ask user to input again
+    }
+
+} while (validInput == false);
