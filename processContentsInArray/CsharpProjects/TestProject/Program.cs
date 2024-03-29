09a29da9d3929115af2afc8377c6fcd4b421c790
farazfayyaz <farazfayyazwork@gmail.com>
2024-03-28 23:45:38 -0400

    committing looping logic practice - access string in array

---
+++ processContentsInArray/CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+
+int periodLocation = 0;
+
+for(int i =0; i < myStrings.Length; i++)
+{
+    Console.WriteLine(myStrings[i]);
+}
