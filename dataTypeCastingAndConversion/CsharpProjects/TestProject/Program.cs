25c9e6c5203f480b9f52412888d7a7cda80b7a5d
farazfayyaz <farazfayyazwork@gmail.com>
2024-04-07 20:55:00 -0400

    using TryParse() method

--- dataTypeCastingAndConversion/CsharpProjects/TestProject/Program.cs
+++ dataTypeCastingAndConversion/CsharpProjects/TestProject/Program.cs
@@ -1,43 +1,57 @@
-﻿int first = 2;
-string second = "4";
-string result = first + second;
-Console.WriteLine(result);
+﻿// int first = 2;
+// string second = "4";
+// string result = first + second;
+// Console.WriteLine(result);
 
-int myInt = 3;
-Console.WriteLine($"int: {myInt}");
+// int myInt = 3;
+// Console.WriteLine($"int: {myInt}");
 
-decimal myDecimal = myInt;  //widening conversion
-Console.WriteLine($"decimal: {myDecimal}");
+// decimal myDecimal = myInt;  //widening conversion
+// Console.WriteLine($"decimal: {myDecimal}");
 
-decimal myDecimal = 3.14m;
-Console.WriteLine($"decimal: {myDecimal}");
+// decimal myDecimal = 3.14m;
+// Console.WriteLine($"decimal: {myDecimal}");
 
-int myInt = (int)myDecimal; //explicit conversion
-Console.WriteLine($"int: {myInt}");
+// int myInt = (int)myDecimal; //explicit conversion
+// Console.WriteLine($"int: {myInt}");
 
-decimal myDecimal = 1.23456789m;
-float myFloat = (float)myDecimal;
+// decimal myDecimal = 1.23456789m;
+// float myFloat = (float)myDecimal;
 
-Console.WriteLine($"Decimal: {myDecimal}");
-Console.WriteLine($"Float  : {myFloat}");
+// Console.WriteLine($"Decimal: {myDecimal}");
+// Console.WriteLine($"Float  : {myFloat}");
 
-int first = 5;
-int second = 7;
-string message = first.ToString() + second.ToString(); // ToString method is a widening conversion
-Console.WriteLine(message);
+// int first = 5;
+// int second = 7;
+// string message = first.ToString() + second.ToString(); // ToString method is a widening conversion
+// Console.WriteLine(message);
 
-string first = "5";
-string second = "7";
-int sum = int.Parse(first) + int.Parse(second); //TryParse is a better method just in case value cannot be converted to int
-Console.WriteLine(sum);
+// string first = "5";
+// string second = "7";
+// int sum = int.Parse(first) + int.Parse(second); //TryParse is a better method just in case value cannot be converted to int
+// Console.WriteLine(sum);
 
-string value1 = "5";
-string value2 = "7";
-int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
-Console.WriteLine(result);
+// string value1 = "5";
+// string value2 = "7";
+// int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+// Console.WriteLine(result);
 
-int value = (int)1.5m; // casting truncates
-Console.WriteLine(value);
+// int value = (int)1.5m; // casting truncates
+// Console.WriteLine(value);
 
-int value2 = Convert.ToInt32(1.5m); // converting rounds up
-Console.WriteLine(value2);
+// int value2 = Convert.ToInt32(1.5m); // converting rounds up
+// Console.WriteLine(value2);
+
+string value = "102";
+int result = 0;
+
+if (int.TryParse(value, out result))
+{
+    Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+    Console.WriteLine("Unable to report the measurement.");
+}
+
+Console.WriteLine($"Measurement (w/ offset): {50 + result}");
--- dataTypeCastingAndConversion/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ dataTypeCastingAndConversion/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c3ea79e372d133c15f8b13b94db6d64ca8514ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81fe47a00d10933752994dc1abfb637ef32b8f2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
