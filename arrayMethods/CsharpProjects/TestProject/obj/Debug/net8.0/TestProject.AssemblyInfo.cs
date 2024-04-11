b6ad3dd63594ede817164d9c0e868140097fb7db
farazfayyaz <farazfayyazwork@gmail.com>
2024-04-10 22:15:41 -0400

    using the array sort method

--- arrayMethods/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ arrayMethods/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ab60bfc4adcccf0af478af68325cbc82e293e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a8c534be00ade73216c9c48c7b369dbfddd2c1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
