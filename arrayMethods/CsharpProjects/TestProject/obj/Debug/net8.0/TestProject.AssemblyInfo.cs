34cdb85e4ccaf4d00de7c64a3b2add3da990ded7
farazfayyaz <farazfayyazwork@gmail.com>
2024-04-11 22:48:09 -0400

    using array clear method

--- arrayMethods/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ arrayMethods/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75d4e2efae7f5acacaa1ec98e7c37f41b4b8933b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7415d6727b7f74c1b0f4e1c8422e0887dce546db")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
