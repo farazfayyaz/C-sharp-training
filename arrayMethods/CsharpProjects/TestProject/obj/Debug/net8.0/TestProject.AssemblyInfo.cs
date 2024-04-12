fe26be9bc73454f3cb16e209b7dbce37666c3b10
farazfayyaz <farazfayyazwork@gmail.com>
2024-04-12 01:14:24 -0400

    using a string

--- arrayMethods/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ arrayMethods/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7415d6727b7f74c1b0f4e1c8422e0887dce546db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c23939913590990b13219e0db2876a13cc06a0b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
