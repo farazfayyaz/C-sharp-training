5fcf0ecd408a7e6f383355799a991b74c6e003d6
farazfayyaz <farazfayyazwork@gmail.com>
2024-04-15 23:11:21 -0400

    understanding the format string method

--- formatAlphanumericData/CsharpProjects/TestProject/Program.cs
+++ formatAlphanumericData/CsharpProjects/TestProject/Program.cs
@@ -3,3 +3,7 @@ string second = "World";
 Console.WriteLine($"{first} {second}!");
 Console.WriteLine($"{second} {first}!");
 Console.WriteLine($"{first} {first} {first}!");
+
+decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
