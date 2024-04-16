d0c5992823cd8655b4c87ac98d503cdc313e5e3b
farazfayyaz <farazfayyazwork@gmail.com>
2024-04-15 22:10:51 -0400

    understanding the string interpolation

--- formatAlphanumericData/CsharpProjects/TestProject/Program.cs
+++ formatAlphanumericData/CsharpProjects/TestProject/Program.cs
@@ -1,7 +1,5 @@
 ﻿string first = "Hello";
 string second = "World";
-string result = string.Format("{1} {0}!", first, second);
-Console.WriteLine(result);
-
-Console.WriteLine("{1} {0}!", first, second);
-Console.WriteLine("{0} {0} {0}!", first, second);
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+Console.WriteLine($"{first} {first} {first}!");
