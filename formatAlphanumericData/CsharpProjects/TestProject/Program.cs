810d0ee0f4f6830507f610472f8f6d8cee875d51
farazfayyaz <farazfayyazwork@gmail.com>
2024-04-16 23:13:17 -0400

    understanding the format string method

--- formatAlphanumericData/CsharpProjects/TestProject/Program.cs
+++ formatAlphanumericData/CsharpProjects/TestProject/Program.cs
@@ -6,4 +6,7 @@ Console.WriteLine($"{first} {first} {first}!");
 
 decimal price = 123.45m;
 int discount = 50;
-Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+Console.WriteLine($"Price: {price:C} (Save {discount:C})"); //currency format
+
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N} units"); //numeric format allows for readability
