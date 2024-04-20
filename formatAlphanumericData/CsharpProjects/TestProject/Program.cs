8c938dd3e2ec019c05a0b41176844f7f747fe8bb
farazfayyaz <farazfayyazwork@gmail.com>
2024-04-20 16:01:19 -0400

    utilizing PadRight and PadLeft to create an app that converts data

--- formatAlphanumericData/CsharpProjects/TestProject/Program.cs
+++ formatAlphanumericData/CsharpProjects/TestProject/Program.cs
@@ -42,9 +42,23 @@
 
 //------------------------------------------------------------------------------
 
-string input = "Pad this";
-// Console.WriteLine(input.PadRight(12)); // padleft method adds spacing on the left of the string
+// string input = "Pad this";
+// // Console.WriteLine(input.PadRight(12)); // padleft method adds spacing on the left of the string
 
-// overloaded methods are methods with extra arguments that modify the funtionality of the method slightly
-Console.WriteLine(input.PadLeft(12, '-'));
- Console.WriteLine(input.PadRight(12, '-'));
+// // overloaded methods are methods with extra arguments that modify the funtionality of the method slightly
+// Console.WriteLine(input.PadLeft(12, '-'));
+//  Console.WriteLine(input.PadRight(12, '-'));
+
+//-------------------------------------------------------------------------------
+// Working with Padded Strings - build an app that converts data to legacy file format
+
+string paymentId = "769C";
+string payeeName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,000.00";
+
+var formattedLine = paymentId.PadRight(6);
+formattedLine += payeeName.PadRight(24);
+formattedLine += paymentAmount.PadLeft(10);
+
+Console.WriteLine("1234567890123456789012345678901234567890");
+Console.WriteLine(formattedLine);
