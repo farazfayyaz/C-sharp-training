3926e79cb868f914891965bbda0273de55a9c72e
farazfayyaz <farazfayyazwork@gmail.com>
2024-04-22 22:51:18 -0400

    using IndexOf with examples

--- stringModifyingMethods/CsharpProjects/TestProject/Program.cs
+++ stringModifyingMethods/CsharpProjects/TestProject/Program.cs
@@ -3,5 +3,8 @@
 int openingPosition = message.IndexOf('('); //IndexOf finds the index of a char
 int closingPosition = message.IndexOf(')');
 
-Console.WriteLine(openingPosition);
-Console.WriteLine(closingPosition);
+// Console.WriteLine(openingPosition);
+// Console.WriteLine(closingPosition);
+
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length + 1));
--- stringModifyingMethods/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ stringModifyingMethods/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79893612242778406364f7e82152bbf1df300908")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f56bf27366803e1d831ed257ae64d899fbd3b8a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
