04b1cfa4843cb9b50ddff0b4074f9ce74e630239
farazfayyaz <farazfayyazwork@gmail.com>
2024-04-24 01:21:59 -0400

    using LastIndexOf

--- stringModifyingMethods/CsharpProjects/TestProject/Program.cs
+++ stringModifyingMethods/CsharpProjects/TestProject/Program.cs
@@ -20,14 +20,22 @@
 // int length = closingPosition - openingPosition;
 // Console.WriteLine(message.Substring(openingPosition, length));
 
-string message = "What is the value <span>between the tags</span>?";
+// string message = "What is the value <span>between the tags</span>?";
 
-const string openSpan = "<span>"; //const keyword allows you to define a variable that can never be changed
-const string closeSpan = "</span>";
+// const string openSpan = "<span>"; //const keyword allows you to define a variable that can never be changed
+// const string closeSpan = "</span>";
 
-int openingPosition = message.IndexOf(openSpan);
-int closingPosition = message.IndexOf(closeSpan);
+// int openingPosition = message.IndexOf(openSpan);
+// int closingPosition = message.IndexOf(closeSpan);
 
-openingPosition += openSpan.Length; //determines the length of the openSpan, which can be changed
+// openingPosition += openSpan.Length; //determines the length of the openSpan, which can be changed
+// int length = closingPosition - openingPosition;
+// Console.WriteLine(message.Substring(openingPosition, length));
+
+string message = "(What if) I am (only interested) in the last (set of parentheses)?";
+int openingPosition = message.LastIndexOf('(');
+
+openingPosition += 1;
+int closingPosition = message.LastIndexOf(')');
 int length = closingPosition - openingPosition;
 Console.WriteLine(message.Substring(openingPosition, length));
