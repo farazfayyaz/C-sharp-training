c3f0794800e14ae276bdc768b8acec1e2b0538ac
farazfayyaz <farazfayyazwork@gmail.com>
2024-05-26 14:42:51 -0400

    updating C# extensions and cache

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c86b13f584154dee9f53c58097f0af947ca5b79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+674cb9d9a9f4f218f13864881cebfd341a4e6fe5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
